a6792f8cd69b3dcf17580c06967a9e0b625201db
Furkan <furkan.nul.aalam@indxtechnology.com>
2024-03-13 16:43:42 +0530

    Minor changes.

--- dist/schema/userSchema.js
+++ dist/schema/userSchema.js
@@ -2,5 +2,8 @@ const userSchema = `type User {
     id: ID!
     email: String!
     roles: String!
+    createdAt: String
+    updatedAt: String
+    deletedAt: String
 }`;
 export default userSchema;
--- src/schema/userSchema.ts
+++ src/schema/userSchema.ts
@@ -2,6 +2,9 @@ const userSchema = `type User {
     id: ID!
     email: String!
     roles: String!
+    createdAt: String
+    updatedAt: String
+    deletedAt: String
 }`
 
 export default userSchema
